019ec6000cca1ed3168d88f2083687aa99e11ea6
Jare <jareloto@gmail.com>
2023-02-27 17:18:25 -0500

    Creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/ecommerce/demoecommerce/DemoecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.demoecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/demoecommerce/DemoecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.demoecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
